70d94759a1dae59151cc6b9f9279d247ac3ed1cc
Brakebein <bruschie@hotmail.com>
2022-03-27 21:06:54 +0200

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
-import string from 'rollup-plugin-string';
-import buble from 'rollup-plugin-buble';
+import { string } from 'rollup-plugin-string';
+import buble from '@rollup/plugin-buble';
 
 export default {
-    entry: 'src/index.js',
-    dest: 'dist/wind-gl.js',
-    format: 'umd',
-    moduleName: 'WindGL',
+    input: 'src/index.js',
     plugins: [
         string({include: './src/shaders/*.glsl'}),
         buble()
+    ],
+    output: [
+        {
+            file: 'dist/wind-gl.js',
+            format: 'umd',
+            name: 'WindGL'
+        }
     ]
 };
